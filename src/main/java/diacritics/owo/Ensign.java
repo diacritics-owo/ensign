bef7ab38177a94916727b4a8fce4eeb9f1a324f9
diacritics-owo <176600453+diacritics-owo@users.noreply.github.com>
2024-08-17 12:28:38 +0530

    modify loot tables in code

--- src/main/java/diacritics/owo/Ensign.java
+++ src/main/java/diacritics/owo/Ensign.java
@@ -1,12 +1,17 @@
 package diacritics.owo;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.block.BannerBlock;
 import net.minecraft.component.ComponentType;
+import net.minecraft.loot.function.CopyComponentsLootFunction;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.resource.ResourceType;
+import net.minecraft.util.DyeColor;
 import net.minecraft.util.Identifier;
+import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import diacritics.owo.component.type.BannerTypeComponent;
@@ -26,6 +31,14 @@ public class Ensign implements ModInitializer {
 
 		ResourceManagerHelper.get(ResourceType.SERVER_DATA)
 				.registerReloadListener(new EnsignResources());
+
+		LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
+			if (source.isBuiltin() && Arrays.stream(DyeColor.values())
+					.anyMatch(color -> BannerBlock.getForColor(color).getLootTableKey().equals(key))) {
+				tableBuilder.apply(CopyComponentsLootFunction
+						.builder(CopyComponentsLootFunction.Source.BLOCK_ENTITY).include(BANNER_TYPE).build());
+			}
+		});
 	}
 
 	public static Identifier identifier(String path) {
